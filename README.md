c10e44c8ecba273b3c342c3dcc60b3d73fbe363b
Subba Reddy Venna <subba.reddy.venna@ing.com>
2021-05-04 18:15:37 +0200

    defined first test case

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # 2021-DEV1-068
 
+Assumptions:
+Two players (humans) are playing one after other on 3x3 board
+First player use 'X' and second player use 'O' letter to reserve the position
+First player always goes first. 
+
+
 Following framework/technologies used :
 1) Java 11
 2) Maven
--- src/test/java/be/app/TicTacToeGameTest.java
+++ src/test/java/be/app/TicTacToeGameTest.java
@@ -1,5 +1,37 @@
 package be.app;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class TicTacToeGameTest {
 
+    TicTacToeGame ticTacToeGame;
+
+
+    @BeforeEach
+    void resetObj(){
+        ticTacToeGame = new TicTacToeGame();
+    }
+
+    @Test
+    public void firstPlayerOwnTheGame(){
+
+        String data = "1"+ System.getProperty("line.separator")
+                +"5"+ System.getProperty("line.separator")
+                +"9"+ System.getProperty("line.separator")
+                +"8"+ System.getProperty("line.separator")
+                +"7"+ System.getProperty("line.separator")
+                +"6"+ System.getProperty("line.separator")
+                +"4"+ System.getProperty("line.separator");
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        String response = ticTacToeGame.startGame();
+        assertThat("Winner is :X").isEqualTo(response);
+    }
 }
